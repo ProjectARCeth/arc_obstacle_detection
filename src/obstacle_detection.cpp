308adada96b43ae87a8121a7dd845f8be79a36ef
Messikommer <nico.messikommer@gmail.com>
2016-12-08 22:44:31 +0100

    classHistogram

--- src/obstacle_detection.cpp
+++ src/obstacle_detection.cpp
@@ -18,16 +18,30 @@ Obstacle_Detection::Obstacle_Detection(const ros::NodeHandle &nh,
 
   ROS_INFO("Obstacle_Detection: initialized!");
 
+}
+
+Obstacle_Detection::~Obstacle_Detection() {
+}
+
+DistanceHistogram::DistanceHistogram() {
   d_histo_ptr_[0] = &d_histo_15_;
   d_histo_ptr_[1] = &d_histo_13_;
   d_histo_ptr_[2] = &d_histo_11_;
   d_histo_ptr_[3] = &d_histo_9_;
   d_histo_ptr_[4] = &d_histo_7_;
   d_histo_ptr_[5] = &d_histo_5_;
+  d_histo_ptr_[6] = &d_histo_3_;
 
 }
 
-Obstacle_Detection::~Obstacle_Detection() {
+DistanceHistogram::~DistanceHistogram() {
+  d_histo_15_.clear();
+  d_histo_13_.clear();
+  d_histo_11_.clear();
+  d_histo_9_.clear();
+  d_histo_7_.clear();
+  d_histo_5_.clear();
+  d_histo_3_.clear();
 }
 
 void Obstacle_Detection::functionCallback(
@@ -55,8 +69,9 @@ void Obstacle_Detection::conversion_PCLtoPC2(
 
 }
 
-void Obstacle_Detection::histogram_allocation(double d, int j) {
-  std::vector < std::vector<double> > &d_histo_temp = *(d_histo_ptr_[j]);
+void Obstacle_Detection::histogram_allocation(double d, int j,
+                                              DistanceHistogram& temp) {
+  std::vector < std::vector<double> > &d_histo_temp = *(temp.d_histo_ptr_[j]);
 //Store
   if (d_histo_temp.size() == 0) {
     std::vector<double> v_dist;
@@ -88,21 +103,20 @@ void Obstacle_Detection::histogram_allocation(double d, int j) {
 void Obstacle_Detection::Filter(
     pcl::PointCloud<pcl::PointXYZ>& filtered_cloud,
     const pcl::PointCloud<pcl::PointXYZ>& temp_cloud, double* inter_d_ptr) {
+
   for (int i = 0; i < temp_cloud.size(); i++) {
     double x = temp_cloud.points[i].x;
     double y = temp_cloud.points[i].y;
     double z = temp_cloud.points[i].z;
     //same condition as filter
-    if (x > 0 && -y_limit_m_ < y && y < y_limit_m_ && z < 0) {
+    if ((x > 0) && (y < x) && (y > -x)) {
 
       double check_d = sqrt(x * x + y * y + z * z);
       double alpha_deg = asin(z / check_d) / M_PI * 180;
       bool angle_assigned = false;
-      for (int j = 0; (!angle_assigned) && (j < 6); j++) {
+      for (int j = 0; (!angle_assigned) && (j < 7); j++) {
         //Check the angle
         if (-15.1 + 2 * j < alpha_deg && alpha_deg < -14.9 + 2 * j) {
-          std::vector < std::vector<double> > &d_histo_temp =
-              *(d_histo_ptr_[j]);
           if (!(*(inter_d_ptr + j) - tolerance_m_ < check_d
               && check_d < *(inter_d_ptr + j) + tolerance_m_)) {
             filtered_cloud.push_back(temp_cloud.points[i]);
@@ -116,11 +130,12 @@ void Obstacle_Detection::Filter(
 
 void Obstacle_Detection::scan(const sensor_msgs::PointCloud2& cloud_message) {
 
-  Clock clock;
-
-  pcl::PointCloud < pcl::PointXYZ > filtered_cloud;
+  //Clock clock;
   pcl::PointCloud < pcl::PointXYZ > temp_cloud;
   conversion_PC2toPCL(cloud_message, temp_cloud);
+
+  DistanceHistogram Front;
+  DistanceHistogram Back;
 
   for (int i = 0; i < temp_cloud.size(); i++) {
     bool angle_assigned = false;
@@ -128,16 +143,21 @@ void Obstacle_Detection::scan(const sensor_msgs::PointCloud2& cloud_message) {
     double y = temp_cloud.points[i].y;
     double z = temp_cloud.points[i].z;
     //check Range limit
-    if (x > 0 && -y_limit_m_ < y && y < y_limit_m_ && z < 0) {
+    if (x > 0 && -y_limit_m_ < y && y < y_limit_m_) {
       double d = sqrt(x * x + y * y + z * z);
       double alpha_deg = asin(z / d) / M_PI * 180;
-      for (int j = 0; (!angle_assigned) && (j < 6); j++) {
+      for (int j = 0; (!angle_assigned) && (j < 7); j++) {
         //search for the right angle
-        if ((-15.1 + 2 * j < alpha_deg) && (alpha_deg < -14.9 + 2 * j)) {
+        if ((-15.1 + 2 * j < alpha_deg)&&(alpha_deg < -14.9 + 2 * j))
+        {
           //allocate an distance intevall for the point
-          histogram_allocation(d, j);
+          histogram_allocation(d, j, Front);
           angle_assigned = true;
         }
+        /*if ((-15.1 + 2 * j < alpha_deg) && (alpha_deg < -14.9 + 2 * j)) {
+         histogram_allocation(d, j, Back);
+         angle_assigned = true;
+         }*/
       }
 
     }
@@ -147,7 +167,8 @@ void Obstacle_Detection::scan(const sensor_msgs::PointCloud2& cloud_message) {
   double inter_d[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };  //distance most points had
 
   for (int i = 0; i < 6; i++) {
-    std::vector < std::vector<double> > &d_histo_temp = *(d_histo_ptr_[i]);
+    std::vector < std::vector<double> > &d_histo_temp =
+        *(Front.d_histo_ptr_[i]);
     //Search distance intervall, in which most points are found
     for (int j = 0; j < d_histo_temp.size(); j++) {
       if (d_histo_temp[j][1] > sum[i]) {
@@ -159,24 +180,19 @@ void Obstacle_Detection::scan(const sensor_msgs::PointCloud2& cloud_message) {
   }
 
 //std::cout << "sum " << sum[1] << "   size    " << d_histo_15.size()<< "   distance  " << inter_d[1] << std::endl;
+
+  pcl::PointCloud < pcl::PointXYZ > filtered_cloud;
   Filter(filtered_cloud, temp_cloud, inter_d);
 
-  //only for visualisation
+//only for visualisation
   sensor_msgs::PointCloud2 converted;
   conversion_PCLtoPC2(filtered_cloud, converted);
   converted.header.stamp = ros::Time::now();
   converted.header.frame_id = cloud_message.header.frame_id;
   obstacle_pub_.publish(converted);
 
-  d_histo_15_.clear();
-  d_histo_13_.clear();
-  d_histo_11_.clear();
-  d_histo_9_.clear();
-  d_histo_7_.clear();
-  d_histo_5_.clear();
-
-  //clock.takeTime();
-  //std::cout<<"Took " << clock.getRealTime() << " ms to filter the input scan."<<std::endl;
+//clock.takeTime();
+//std::cout<<"Took " << clock.getRealTime() << " ms to filter the input scan."<<std::endl;
 
 }
 
