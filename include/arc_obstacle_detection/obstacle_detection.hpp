2d3c43d7b19ba2c7d1d8c9450e4cda4ba4a9ae87
Messikommer <nico.messikommer@gmail.com>
2017-02-22 11:37:58 +0100

    shutdown

--- include/arc_obstacle_detection/obstacle_detection.hpp
+++ include/arc_obstacle_detection/obstacle_detection.hpp
@@ -11,6 +11,7 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include "std_msgs/Bool.h"
 
 namespace arc {
 namespace obstacle_detection {
@@ -48,6 +49,8 @@ class Obstacle_Detection {
               double* inter_d_ptr);
   void GridMap(pcl::PointCloud<pcl::PointXYZ>& filtered_cloud,
                nav_msgs::OccupancyGrid& grid);
+  
+  void shut_down(const std_msgs::Bool::ConstPtr& msg);
 
  private:
   ros::NodeHandle nh_;
@@ -55,66 +58,13 @@ class Obstacle_Detection {
   ros::Publisher obstacle_pub_;
   ros::Publisher gridmap_pub_;
   ros::Subscriber obstacle_sub_;
+  ros::Subscriber shutdown_sub_;
   double y_limit_m_;
   double tolerance_m_;
   double tolerance_factor_;
 
 };
 
-//Copied from segmatch, difference: constexpr - const
-/*
- class Clock {
-
- public:
- Clock() {
- start();
- }
-
- /// \brief Start clock timer.
- void start() {
- gettimeofday(&real_time_start_, NULL);
- cpu_start_ = clock();
- }
-
- /// \brief Sample clock timer.
- void takeTime() {
- struct timeval end;
- gettimeofday(&end, NULL);
- cpu_time_ms_ = double(clock() - cpu_start_) / CLOCKS_PER_SEC
- * kSecondsToMiliseconds;
-
- long seconds, useconds;
-
- seconds = end.tv_sec - real_time_start_.tv_sec;
- useconds = end.tv_usec - real_time_start_.tv_usec;
- real_time_ms_ = (seconds * kSecondsToMiliseconds
- + useconds * kMicrosecondsToMiliseconds) + 0.5;
- }
-
- /// \brief Return elapsed physical time.
- double getRealTime() {
- return real_time_ms_;
- }
-
- /// \brief Return elapsed CPU time.
- double getCPUTime() {
- return cpu_time_ms_;
- }
-
- double takeRealTime() {
- takeTime();
- return getRealTime();
- }
-
- private:
- struct timeval real_time_start_;
- double real_time_ms_, cpu_time_ms_;
- clock_t cpu_start_;
-
- static const double kSecondsToMiliseconds = 1000.0;
- static const double kMicrosecondsToMiliseconds = 0.001;
- };
- */
 }  //End obstacle_detection
 }  //End arc
 
